44dce6017179c75702d9fd483714fc357a5a19c7
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2020-12-23 18:04:21 +0300

    Update metaclasses
    combine()
      Added combined classes caching.
      If one of bases is a combination class, its bases are used instead of
      it.
      Name of the resulting combination class now consists of bases'
      qualnames joint by @.
    CombineMeta
      All matmul are using combine().
    Singleton
      Fixed incorrect super().__call__() call.
      Now does not accepts call_init kw, It is job of SingletonWithInit.
    To Singleton and EmptySlotsByDefaults added kw accepting and passing tio
     super().
    Added AllowInstantiation class.

--- common/metaclasses.py
+++ common/metaclasses.py
@@ -1,9 +1,30 @@
 from typing import final
-from weakref import WeakKeyDictionary
+from weakref import WeakKeyDictionary, WeakValueDictionary
+
+_combined_metaclasses = WeakValueDictionary()
 
 
 def combine(*classes: type):
-    return CombineMeta(''.join(cls.__name__ for cls in classes), classes, {})
+    real_classes = {}  # using as ordered set
+    for cls in classes:
+        if cls.__bases__ in _combined_metaclasses:  # indicates that cls is a combination class
+            for base in cls.__bases__:
+                # No need to check bases of bases
+                # This function guaranties that any of combination class's bases is not a combination class
+                real_classes[base] = None
+        else:
+            real_classes[cls] = None
+    classes = tuple(real_classes)
+
+    # Order of bases matters
+    # (A, B) and (B, A) different base tuples which lead to different MRO
+    # So combined classes of these bases are different
+    if classes in _combined_metaclasses:
+        return _combined_metaclasses[classes]
+
+    cls = CombineMeta('@'.join(cls.__qualname__ for cls in classes), classes, {})
+    _combined_metaclasses[classes] = cls
+    return cls
 
 
 @final
@@ -15,10 +36,10 @@ class CombineMeta(type):
     combine = staticmethod(combine)
 
     def __matmul__(self, other):
-        return CombineMeta(self.__name__ + other.__name__, (self, other), {})
+        return combine(self, other)
 
     def __rmatmul__(self, other):
-        return CombineMeta(other.__name__ + self.__name__, (other, self), {})
+        return combine(other, self)
 
     def __init_subclass__(mcs, **kwargs):
         raise TypeError(f'type MetaOfMeta is not an acceptable base type')
@@ -27,29 +48,54 @@ class CombineMeta(type):
 class Singleton(type, metaclass=CombineMeta):
     # Use weak keys to automatically clean unused classes
     __instances__ = WeakKeyDictionary()
-    __call_init__ = WeakKeyDictionary()
-
-    def __new__(mcs, name: str, bases: tuple, namespace: dict, *, call_init: bool = False):
-        cls = super().__new__(mcs, name, bases, namespace)
-        if not isinstance(call_init, bool):
-            raise TypeError(f'call_init argument must be bool, got {type(call_init)}')
-
-        mcs.__call_init__[cls] = call_init
-        return cls
 
     def __call__(cls, *args, **kwargs):
         instances = cls.__class__.__instances__
-        call_init = cls.__class__.__call_init__[cls]
         if cls not in instances:
-            instances[cls] = super().__call__(cls, *args, **kwargs)
-        elif call_init:
-            instances[cls].__init__(*args, **kwargs)
+            instances[cls] = super().__call__(*args, **kwargs)
 
         return instances[cls]
 
 
+class SingletonWithInit(Singleton):
+    __call_init__ = WeakKeyDictionary()
+
+    def __new__(mcs, name: str, bases: tuple, namespace: dict, *, call_init: bool = False, **kwargs):
+        if not isinstance(call_init, bool):
+            raise TypeError(f'call_init argument must be bool, got {type(call_init)}')
+
+        cls = super().__new__(mcs, name, bases, namespace, **kwargs)
+        mcs.__call_init__[cls] = call_init
+        return cls
+
+    def __call__(cls, *args, **kwargs):
+        instance = super().__call__(*args, **kwargs)
+        if cls.__class__.__call_init__[cls]:
+            instance.__init__(*args, **kwargs)
+
+        return instance
+
+
 class EmptySlotsByDefaults(type, metaclass=CombineMeta):
-    def __new__(mcs, name: str, bases: tuple, namespace: dict):
+    def __new__(mcs, name: str, bases: tuple, namespace: dict, **kwargs):
         if '__slots__' not in namespace:
             namespace['__slots__'] = ()
-        return super().__new__(mcs, name, bases, namespace)
+        return super().__new__(mcs, name, bases, namespace, **kwargs)
+
+
+class AllowInstantiation(type, metaclass=CombineMeta):
+    __allow__ = WeakKeyDictionary()
+
+    def __new__(mcs, name: str, bases: tuple, namespace: dict, *, allow_instances: bool = True, **kwargs):
+        if not isinstance(allow_instances, bool):
+            raise TypeError(f'allow_instances argument must be bool, got {type(allow_instances)}')
+
+        cls = super().__new__(mcs, name, bases, namespace, **kwargs)
+        mcs.__allow__[cls] = allow_instances
+        return cls
+
+    def __call__(cls, *args, **kwargs):
+        if cls.__class__.__allow__[cls]:
+            return super().__call__(*args, **kwargs)
+
+        raise TypeError(f'class {cls.__class__.__qualname__} cannot be instantiated')
