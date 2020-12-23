6e8229225f93785c6287f535d2c51ffe4d43a390
Prometheus <prometheus3375@gmail.com>
2020-12-23 15:24:25 +0300

    Add NamedTuple check functions

--- common/functions.py
+++ common/functions.py
@@ -9,3 +9,20 @@ def except_(smth: Callable[..., Any], *args, **kwargs):
         message = ''.join(format_tb(e.__traceback__))
         message = f'Traceback (most recent call last):\n{message}{e.__class__.__name__}: {e}\n'
         print(message)
+
+
+def isnamedtuplesubclass(cls: type) -> bool:
+    # https://stackoverflow.com/a/2166841
+    bases = cls.__bases__
+    if len(bases) != 1 or bases[0] is not tuple:
+        return False
+
+    fields = getattr(cls, '_fields', None)
+    if not isinstance(fields, tuple):
+        return False
+
+    return all(type(n) is str for n in fields)
+
+
+def isnamedtuple(obj: tuple) -> bool:
+    return isnamedtuplesubclass(obj.__class__)
