f10f87fa462c755fbeef2322ff115d250710ca84
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2020-12-24 21:47:15 +0300

    Update BijectiveMap
    Added views.
    Added common mapping methods.
    contains() accepts pairs instead of values.
    iter() return iterator over pairs.
    values() and items() returns correspondent views.
    repr() converts pairs to strings via repr().

--- common/biject.py
+++ common/biject.py
@@ -1,5 +1,4 @@
-from collections import Iterable
-from typing import Collection, Generic, Iterator, TypeVar, Union
+from typing import AbstractSet, Generic, Iterable, Iterator, TypeVar, Union, overload
 
 _T1 = TypeVar('_T1')
 _T2 = TypeVar('_T2')
@@ -7,13 +6,51 @@ _K = Union[_T1, _T2]
 _V = tuple[_T1, _T2]
 
 
-class BijectiveMap(Generic[_T1, _T2], Collection):
+class BijectiveMapValuesView(AbstractSet[_K]):
+    __slots__ = '_source'
+
+    def __init__(self, source: 'BijectiveMap'):
+        self._source = source
+        super().__init__()
+
+    def __len__(self) -> int:
+        return len(self._source)
+
+    def __iter__(self) -> Iterator[_K]:
+        for v1, v2 in self._source:
+            yield v1
+            yield v2
+
+    def __contains__(self, value: _K) -> bool:
+        return self._source.have_value(value)
+
+
+class BijectiveMapItemsView(AbstractSet[_V]):
+    __slots__ = '_source'
+
+    def __init__(self, source: 'BijectiveMap'):
+        self._source = source
+        super().__init__()
+
+    def __len__(self) -> int:
+        return len(self._source)
+
+    def __iter__(self) -> Iterator[_V]:
+        return iter(self._source)
+
+    def __contains__(self, value: _V) -> bool:
+        return value in self._source
+
+
+class BijectiveMap(Generic[_T1, _T2]):
     __slots__ = '_data'
+
+    _no_arg = object()
 
     def __init__(self, iterable: Iterable[_V] = ()):
         self._data: dict[_K, _V] = {}
-        for t1, t2 in iterable:
-            self.update(t1, t2)
+        for v1, v2 in iterable:
+            self.update(v1, v2)
 
     def _add(self, v1: _T1, v2: _T2):
         t = v1, v2
@@ -38,38 +75,105 @@ class BijectiveMap(Generic[_T1, _T2], Collection):
             self.__delitem__(v2)
         self._add(v1, v2)
 
-    def __delitem__(self, value: _K):
-        t1, t2 = self._data[value]
-        if value is t1 or value == t1 or value is t2 or value == t2:
-            del self._data[t1]
-            del self._data[t2]
-        else:
-            raise KeyError(f'Key {value} points to tuple ({t1}, {t2}) which does contain it')
+    @overload
+    def get(self, value: _T1) -> _T2: ...
+
+    @overload
+    def get(self, value: _T2) -> _T1: ...
+
+    @overload
+    def get(self, value: _T1, default: _T2) -> _T2: ...
+
+    @overload
+    def get(self, value: _T2, default: _T1) -> _T1: ...
+
+    def get(self, value: _K, default: _K = _no_arg) -> _K:
+        v1, v2 = self._data[value]
+        if value is v1 or value == v1:
+            return v2
+
+        if value is v2 or value == v2:
+            return v1
+
+        if default is self._no_arg:
+            raise KeyError(f'Key {value} points to tuple ({v1}, {v2}) which does not contain it')
+
+        return default
+
+    @overload
+    def __getitem__(self, value: _T1) -> _T2: ...
+
+    @overload
+    def __getitem__(self, value: _T2) -> _T1: ...
 
     def __getitem__(self, value: _K) -> _K:
-        t1, t2 = self._data[value]
-        if value is t1 or value == t1:
-            return t2
+        return self.get(value)
 
-        if value is t2 or value == t2:
-            return t1
+    @overload
+    def pop(self, value: _T1) -> _T2: ...
 
-        raise KeyError(f'Key {value} points to tuple ({t1}, {t2}) which does contain it')
+    @overload
+    def pop(self, value: _T2) -> _T1: ...
+
+    @overload
+    def pop(self, value: _T1, default: _T2) -> _T2: ...
+
+    @overload
+    def pop(self, value: _T2, default: _T1) -> _T1: ...
+
+    def pop(self, value: _K, default: _K = _no_arg) -> _K:
+        v = self.get(value, default)
+        self.__delitem__(v)
+        return v
+
+    def popitem(self) -> _V:
+        value, (v1, v2) = self._data.popitem()
+        if value is v1 or value == v1:
+            del self._data[v2]
+        elif value is v2 or value == v2:
+            del self._data[v1]
+
+        return v1, v2
+
+    def __delitem__(self, value: _K):
+        v1, v2 = self._data[value]
+        if value is v1 or value == v1 or value is v2 or value == v2:
+            del self._data[v1]
+            del self._data[v2]
+        else:
+            raise KeyError(f'Key {value} points to tuple ({v1}, {v2}) which does not contain it')
 
     def __len__(self) -> int:
         return len(self._data) // 2
 
-    def __contains__(self, value: _K):
+    def __contains__(self, item: _V) -> bool:
+        v1, v2 = item
+        return v1 in self._data and v2 in self._data
+
+    # Not possible to integrate this method into __contains__
+    # _T1 or _T2 can be tuples of size 2
+    # In such case, it is not possible to determine
+    # whether _K or _V is passed
+    def has_value(self, value: _K) -> bool:
         return value in self._data
 
-    def values(self) -> set[_K]:
-        return set(self._data.keys())
+    def __iter__(self) -> Iterator[_V]:
+        visited = set()
+        for item in self._data.items():
+            if item in visited:
+                continue
 
-    def items(self) -> set[_V]:
-        return set(self._data.values())
+            visited.add(item)
+            yield item
 
-    def __iter__(self) -> Iterator[_K]:
-        return iter(self._data.keys())
+    def values(self) -> BijectiveMapValuesView[_K]:
+        return BijectiveMapValuesView(self)
+
+    def items(self) -> BijectiveMapItemsView[_V]:
+        return BijectiveMapItemsView(self)
+
+    def clear(self):
+        self._data.clear()
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
@@ -83,9 +187,6 @@ class BijectiveMap(Generic[_T1, _T2], Collection):
 
         raise TypeError(f'An attempt to compare types {type(self)} and {type(other)}')
 
-    def clear(self):
-        self._data.clear()
-
     def __repr__(self):
-        entries = ', '.join(str(v) for v in self.items())
+        entries = ', '.join(repr(v) for v in self)
         return f'{self.__class__.__name__}[{entries}]'
