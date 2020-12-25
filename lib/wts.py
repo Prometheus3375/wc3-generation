5fc612d41ca9d0bfcd979109ba827ecb7312ffd7
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2020-12-25 03:08:23 +0300

    Major wts.py update
    wtsString:
      id and comment are immutable.
      Added comment_data property to extract useful info from comments.
      Added repr().
      Added lt() for sorting() support.
    
    Added CommentMap for mapping useful comment info to source strings.
    Added wtsReadError to raise on obvious wts read error.
    
    wtsFile:
      _strings is dict now.
      Added _comment_map.
      New has_id() and find().
      id_ in read() is zero by default.

--- lib/wts.py
+++ lib/wts.py
@@ -5,29 +5,14 @@ TODO
 Сделать класс wtsStorage, который парсит .wts файл и извлекает из него строки, а также будет иметь методы по поиску
 и сохранению в файл.
 
+Зарепортить баг о сбросе настройки инспекций проекта.
+
 """
 from enum import Enum
 from io import StringIO
-from typing import ClassVar, Iterator, final, overload
+from typing import ClassVar, Iterator, Optional, final
 
-
-@final
-class wtsString:
-    __slots__ = 'id', 'comment', 'content'
-
-    def __init__(self, id_: int, comment: str, content: str):
-        self.id = id_
-        self.comment = comment
-        self.content = content
-
-    def __str__(self):
-        return (
-            f'STRING {self.id}\n'
-            f'{self.comment}'
-            '{\n'
-            f'{self.content}'
-            '}\n\n'
-        )
+from common import truncate_string
 
 
 @final
@@ -74,19 +59,148 @@ class CommentField(Enum):
     Ubertip = 'Ubertip'
 
 
+CommentData = tuple[CommentType, str, CommentField]
+
+
+@final
+class wtsString:
+    __slots__ = '_id', '_comment', '_content'
+
+    def __init__(self, id_: int, comment: str, content: str):
+        self._id = id_
+        self._comment = comment
+        self._content = content
+
+    @property
+    def id(self) -> int:
+        return self._id
+
+    @property
+    def comment(self) -> str:
+        return self._comment
+
+    @property
+    def comment_data(self) -> Optional[CommentData]:
+        comment = self.comment.strip()
+        if not comment:
+            return None
+
+        colon = comment.index(':')
+        typ = CommentType(comment[3:colon])
+        rawcode = comment[colon + 2:colon + 6]
+        closing_brace = comment.rindex(')', colon + 8, len(comment) - 2)
+        opening_brace = comment.index('(', closing_brace + 3)
+        field = CommentField(comment[closing_brace + 3:opening_brace - 1])
+        return typ, rawcode, field
+
+    @property
+    def content(self):
+        return self._content
+
+    @content.setter
+    def content(self, value: str):
+        self._content = value
+
+    def __str__(self) -> str:
+        return (
+            f'STRING {self.id}\n'
+            f'{self.comment}'
+            '{\n'
+            f'{self.content}'
+            '}\n\n'
+        )
+
+    def __repr__(self) -> str:
+        return (
+            f'{self.__class__.__name__}('
+            f'id={self._id}, '
+            f'comment=\'{truncate_string(self._comment[3:].strip())}\', '
+            f'content=\'{truncate_string(self._content.strip())}\''
+            f')'
+        )
+
+    def __lt__(self, other: object):
+        if isinstance(other, self.__class__):
+            return self._id < other._id
+
+        raise TypeError(f"'<' is not supported between instances "
+                        f"of '{self.__class__.__name__}' and '{other.__class__.__name__}'")
+
+
+@final
+class CommentMap:
+    __slots__ = '_map',
+
+    def __init__(self):
+        self._map: dict[CommentData, list[wtsString]] = {}
+
+    def add(self, value: wtsString) -> bool:
+        data = value.comment_data
+        if data:
+            if data in self._map:
+                self._map[data].append(value)
+            else:
+                self._map[data] = [value]
+
+            return True
+
+        return False
+
+    def __getitem__(self, item: CommentData) -> Optional[list[wtsString]]:
+        return self._map.get(item, None)
+
+    def __len__(self) -> int:
+        return len(self._map)
+
+    def __contains__(self, item: CommentData) -> bool:
+        return item in self._map
+
+    def __iter__(self) -> Iterator[CommentData]:
+        return iter(self._map)
+
+    def clear(self):
+        self._map.clear()
+
+
+@final
+class wtsReadError(Exception):
+    __module__ = 'builtins'
+
+
 @final
 class wtsFile:
-    __slots__ = '_filepath', '_strings'
+    __slots__ = '_filepath', '_strings', '_comment_map'
 
     encoding: ClassVar[str] = 'utf-8-sig'
 
     def __init__(self, filepath: str):
         self._filepath = filepath
-        self._strings: list[wtsString] = []
+        self._strings: dict[int, wtsString] = {}
+        self._comment_map = CommentMap()
+
+    @property
+    def filepath(self) -> str:
+        return self._filepath
+
+    def __len__(self) -> int:
+        return len(self._strings)
+
+    def __contains__(self, wts: wtsString) -> bool:
+        return wts.id in self._strings and self._strings[wts.id] is wts
+
+    def has_id(self, id_: int) -> bool:
+        return id_ in self._strings
+
+    def __iter__(self) -> Iterator[wtsString]:
+        return iter(sorted(self._strings.values()))
+
+    def __getitem__(self, id_: int) -> wtsString:
+        return self._strings[id_]
 
     def read(self):
         self._strings.clear()
-        id_ = -1
+        self._comment_map.clear()
+        id_ = 0
         comment = StringIO()
         content = StringIO()
         outside = True  # outside of content block
@@ -100,9 +214,17 @@ class wtsFile:
                     elif line.startswith('STRING '):
                         id_ = int(line.strip()[7:])
                 elif line.startswith('}'):
-                    self._strings.append(wtsString(id_, comment.getvalue(), content.getvalue()))
-                    id_ = -1
-                    # Why recreating better https://stackoverflow.com/a/4330829
+                    if id_ <= 0:
+                        raise wtsReadError(f'Cannot create {wtsString.__name__} with non-positive id: {id_}')
+                    if content.tell() == 0:
+                        raise wtsReadError(f'Cannot create {wtsString.__name__} with empty content')
+
+                    wts = wtsString(id_, comment.getvalue(), content.getvalue())
+                    self._strings[wts.id] = wts
+                    self._comment_map.add(wts)
+
+                    id_ = 0
+                    # Why recreating is better https://stackoverflow.com/a/4330829
                     comment = StringIO()
                     content = StringIO()
                     outside = True
@@ -113,26 +235,11 @@ class wtsFile:
 
     def save(self):
         with open(self._filepath, 'w', encoding=self.encoding) as f:
-            f.writelines(str(s) for s in self._strings)
+            f.writelines(str(s) for s in self)
 
-    @property
-    def filepath(self) -> str:
-        return self._filepath
+    def find(self, typ: CommentType, rawcode: str, field: CommentField, level: int = 1) -> Optional[wtsString]:
+        strings = self._comment_map[typ, rawcode, field]
+        if strings is None:
+            return None
 
-    def __len__(self) -> int:
-        return len(self._strings)
-
-    def __contains__(self, item: wtsString) -> bool:
-        return item in self._strings
-
-    def __iter__(self) -> Iterator[wtsString]:
-        return iter(self._strings)
-
-    @overload
-    def __getitem__(self, index: int) -> wtsString: ...
-
-    @overload
-    def __getitem__(self, slice_: slice) -> list[wtsString]: ...
-
-    def __getitem__(self, item):
-        return self._strings[item]
+        return strings[level - 1]
