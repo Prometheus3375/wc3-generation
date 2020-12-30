622ac8968c2d7ebea603d99fdc140a1fecd42291
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2020-12-30 21:15:14 +0300

    Add typing module
    This module have useful methods and constants for typing.

---
+++ common/typing.py
@@ -0,0 +1,29 @@
+import types
+import typing
+
+Types_GenericAlias = types.GenericAlias
+Typing_GenericAlias = type(typing.Final[int])
+SpecialForm = type(typing.Union)
+GenericAlias = Types_GenericAlias, Typing_GenericAlias  # for isinstance() and issubclass()
+
+Annotation = typing.Union[
+    type,
+    Types_GenericAlias,
+    Typing_GenericAlias,
+    SpecialForm,
+    typing.TypeVar,
+    # may be incomplete
+]
+
+
+def check_type(typ: Annotation, msg: str, is_argument: bool = True) -> Annotation:
+    """
+    Check that the argument is an annotation, and return it.
+    If is_argument is True, also checks whether the argument is a valid annotation argument
+    """
+    return typing._type_check(typ, msg, is_argument)
+
+
+def repr_type(a: Annotation) -> str:
+    """Return the repr() of an annotation"""
+    return typing._type_repr(a)
