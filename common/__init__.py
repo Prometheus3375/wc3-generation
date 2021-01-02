d0161de6101a579ee47788a478e149e91ac20aba
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2021-01-02 20:21:29 +0300

    Update repr_strings()
    Renamed to repr_collection.
    Accepts any collection.
    Added optional argument delimiter.

--- common/__init__.py
+++ common/__init__.py
@@ -1,3 +1,3 @@
 from .biject import BijectiveMap
 from .frozendict import frozendict
-from .functions import exc2str, except_, isnamedtuple, isnamedtuplesubclass, repr_strings, truncate_string
+from .functions import exc2str, except_, isnamedtuple, isnamedtuplesubclass, repr_collection, truncate_string
