9ec3123b56734595d01ee4a6cf4b6bb624b9eceb
Prometheus <prometheus3375@gmail.com>
2021-01-04 23:56:06 +0300

    Add new function to common

--- common/__init__.py
+++ common/__init__.py
@@ -1,3 +1,11 @@
 from .biject import BijectiveMap
 from .frozendict import frozendict
-from .functions import exc2str, except_, isnamedtuple, isnamedtuplesubclass, repr_collection, truncate_string
+from .functions import (
+    exc2str,
+    except_,
+    isnamedtuple,
+    isnamedtuplesubclass,
+    repr_collection,
+    set_mismatch_message,
+    truncate_string,
+)
