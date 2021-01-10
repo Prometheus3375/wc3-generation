481201b0023f58d37c177fd76ffb360312289437
Prometheus <prometheus3375@gmail.com>
2021-01-10 14:46:08 +0300

    Updated iter_vs_yield.py

--- tests/iter_vs_yield.py
+++ tests/iter_vs_yield.py
@@ -10,11 +10,16 @@ def generator(t: tuple):
 
 
 result = repeat('o = iterator((1, 2, 3, 4, 5, 6, 7, 8, 9))', globals_=dict(iterator=iterator))
-print(result)  # 115.7 ±   26.4 ns
+print(result)  # 127.1 ±   89.5 ns
 result = repeat('o = iterator((1, 2, 3, 4, 5, 6, 7, 8, 9))', globals_=dict(iterator=generator))
-print(result)  # 177.5 ±   37.0 ns
+print(result)  # 183.2 ±   40.6 ns
 
 result = repeat('for _ in o: pass', 'o = iterator((1, 2, 3, 4, 5, 6, 7, 8, 9))', globals_=dict(iterator=iterator))
-print(result)  # 17.8 ±    0.9 ns
+print(result)  # 19.2 ±    7.6 ns
 result = repeat('for _ in o: pass', 'o = iterator((1, 2, 3, 4, 5, 6, 7, 8, 9))', globals_=dict(iterator=generator))
-print(result)  # 20.4 ±    5.4 ns
+print(result)  # 22.6 ±   11.1 ns
+
+result = repeat('for _ in iterator((1, 2, 3, 4, 5, 6, 7, 8, 9)): pass', globals_=dict(iterator=iterator))
+print(result)  # 199.7 ±   39.3 ns
+result = repeat('for _ in iterator((1, 2, 3, 4, 5, 6, 7, 8, 9)): pass', globals_=dict(iterator=generator))
+print(result)  # 478.2 ±   70.7 ns
