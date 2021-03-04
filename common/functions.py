9a80ff5679c77e5550a674f2f78766427985ca45
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2021-03-04 17:07:33 +0300

    Remove all_util.py

--- common/functions.py
+++ common/functions.py
@@ -2,19 +2,15 @@ from collections.abc import Callable, Collection, Iterable
 from traceback import format_tb
 from typing import Any, Union
 
-from all_util import reg
-
 _IterableStr = Union[Iterable[str], str]
 
 
-@reg
 def exc2str(e: Exception) -> str:
     message = ''.join(format_tb(e.__traceback__))
     message = f'Traceback (most recent call last):\n{message}{e.__class__.__name__}: {e}\n'
     return message
 
 
-@reg
 def except_(smth: Callable[..., Any], *args, **kwargs):
     try:
         return smth(*args, **kwargs)
@@ -22,7 +18,6 @@ def except_(smth: Callable[..., Any], *args, **kwargs):
         print(exc2str(e))
 
 
-@reg
 def isnamedtuplesubclass(cls: type) -> bool:
     # https://stackoverflow.com/a/2166841
     bases = cls.__bases__
@@ -36,21 +31,18 @@ def isnamedtuplesubclass(cls: type) -> bool:
     return all(type(n) is str for n in fields)
 
 
-@reg
 def isnamedtuple(obj: tuple) -> bool:
     return isnamedtuplesubclass(obj.__class__)
 
 
-@reg
 def truncate_string(s: str, max_len: int = 10) -> str:
     return s if len(s) <= max_len else f'{s[:max_len - 3]}...'
 
 
-@reg
 def repr_collection(c: Collection, singular: _IterableStr, plural: _IterableStr, /, *,
                     delimiter: str = ', ', use_repr: bool = True) -> tuple[_IterableStr, str]:
     func = repr if use_repr else str
     return singular if len(c) == 1 else plural, delimiter.join(func(o) for o in c)
 
 
-__all__ = *reg(__name__),
+__all__ = 'exc2str', 'except_', 'isnamedtuplesubclass', 'isnamedtuple', 'truncate_string', 'repr_collection'
